4732df84dff47ec1eb544990d39645c971618199
Orsar <orsarba@gmail.com>
2026-01-21 13:47:26 +0100

    Update hr.js

--- hr.js
+++ hr.js
@@ -4,3 +4,9 @@ const navLinks = document.getElementById("nav-links");
 menuToggle.addEventListener("click", () => {
   navLinks.classList.toggle("show");
 });
+
+document.querySelectorAll("#nav-links a").forEach(link => {
+  link.addEventListener("click", () => {
+    navLinks.classList.remove("show");
+  });
+});
